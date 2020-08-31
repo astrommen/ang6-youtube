41b36291e6ecf2ffb1616f08859dc1fc89d536e0
Aja <a.g.mechanix>
2020-08-30 23:40:17 -0500

    fixed ts error 2339

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
+
 export class DetailsComponent implements OnInit {
 
   user$: Object;
@@ -18,8 +19,18 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit() {
     this.data.getUser(this.user$).subscribe(
-      data => this.user$ = data
-    )
+      //enclosed in brackets for testing
+      (data:any) => {this.user$ = data
+        // console.log(this.user$.name);
+      }
+    );
   }
+}
 
+//had to declare property types to get rid of error TS2339
+interface Object {
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
 }
