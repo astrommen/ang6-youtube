8d6aa2e0ba5dd79c5c6b5c6a93bbb6b733c613fc
Aja <a.g.mechanix>
2020-09-01 15:47:35 -0500

    class binding css style for page indication

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
+
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  currentUrl: string;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    console.log(router.url);
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      // casting event before accessing url from https://github.com/angular/angular/issues/15439
+      this.currentUrl = (event as NavigationEnd).url;
+    })
+    
   }
-
+  
+  ngOnInit() {
+  }
 }
+  
+  
+
